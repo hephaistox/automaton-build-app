67a035b6b534c08e301d6f9606b3ec65e9bae024
caumond <anthony@caumond.com>
2023-10-21 17:39:01 +0200

    test

--- src/clj/automaton_build_app/cicd/version.clj
+++ src/clj/automaton_build_app/cicd/version.clj
@@ -1,10 +1,8 @@
 (ns automaton-build-app.cicd.version
   "Version of the current codebase"
   (:require [automaton-build-app.log :as build-log]
+            [automaton-build-app.os.edn-utils :as build-edn-utils]
             [automaton-build-app.os.files :as build-files]))
-
-(def dir "")
-(def version-filename (build-files/create-file-path dir "version.edn"))
 
 (defn version-to-push
   "Build the string of the version to be pushed (the next one)
@@ -14,11 +12,20 @@
   [dir major-version]
   (if major-version
     (let [version-filename (build-files/create-file-path dir "version.edn")
-          minor-version (-> version-filename
-                            build-files/read-file
-                            (or -1)
-                            inc)
-          version (format major-version minor-version)]
-      (build-files/spit-file version-filename version)
-      version)
+          version-map (build-edn-utils/read-edn version-filename)
+          {_version :version,
+           older-minor-version :minor-version,
+           older-major-version :major-version}
+            version-map
+          minor-version (when (= older-major-version major-version)
+                          older-minor-version)
+          new-minor-version (inc (or minor-version -1))
+          new-version (format major-version new-minor-version)]
+      (build-edn-utils/spit-edn
+        version-filename
+        {:version new-version,
+         :major-version major-version,
+         :minor-version new-minor-version}
+        "Last generated version, note a failed push consume a number")
+      new-version)
     (build-log/warn "Major version is missing")))
