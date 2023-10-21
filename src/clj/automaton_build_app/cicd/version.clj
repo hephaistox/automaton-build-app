b6fac7a6114aa9a4d49347b330378791c1917bde
caumond <anthony@caumond.com>
2023-10-21 16:57:32 +0200

    test

--- src/clj/automaton_build_app/cicd/version.clj
+++ src/clj/automaton_build_app/cicd/version.clj
@@ -1,17 +1,24 @@
 (ns automaton-build-app.cicd.version
   "Version of the current codebase"
   (:require [automaton-build-app.log :as build-log]
-            [clojure.tools.build.api :as build-build-api]))
+            [automaton-build-app.os.files :as build-files]))
+
+(def dir "")
+(def version-filename (build-files/create-file-path dir "version.edn"))
 
 (defn version-to-push
   "Build the string of the version to be pushed (the next one)
   Params:
-  * `dir` directory where to count the git count of commits
+  * `dir` directory of the repo to count the version of
   * `major-version`"
   [dir major-version]
   (if major-version
-    (->> (build-build-api/git-count-revs {:dir dir})
-         Integer/parseInt
-         inc
-         (format major-version))
+    (let [version-filename (build-files/create-file-path dir "version.edn")
+          minor-version (-> version-filename
+                            build-files/read-file
+                            (or -1)
+                            inc)
+          version (format major-version minor-version)]
+      (build-files/spit-file version-filename version)
+      version)
     (build-log/warn "Major version is missing")))
