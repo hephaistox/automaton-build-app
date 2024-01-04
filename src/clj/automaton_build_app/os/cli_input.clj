3f7bcb3dbb6d5d9b797178dcb0e346d11a99868b
kaspazza <kaspazza@gmail.com>
2024-01-04 21:13:47 +0100

    Force option for user question

--- src/clj/automaton_build_app/os/cli_input.clj
+++ src/clj/automaton_build_app/os/cli_input.clj
@@ -1,7 +1,9 @@
 (ns automaton-build-app.os.cli-input
   (:require [automaton-build-app.log :as build-log]))
 
-(defn yes-question [msg force?]
-  (if force?
-    true
-    (do (build-log/warn msg) (flush) (contains? #{'y 'Y 'yes 'Yes 'YES} (read)))))
+(defn yes-question
+  ([msg force?]
+   (if force?
+     true
+     (do (build-log/warn msg) (flush) (contains? #{'y 'Y 'yes 'Yes 'YES} (read)))))
+  ([msg] (yes-question msg false)))
